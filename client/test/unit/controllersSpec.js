79eccc661db06b0edbf123ce0792251a52a9d8e0
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-19 23:10:45 -0700

    add client directory and working test suite for angular

---
+++ client/test/unit/controllersSpec.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('SignInController', function(){
+  var myCtrl1;
+
+  beforeEach(function(){
+    SignInController = new SignInController();
+  });
+
+
+  it('should ....', function() {
+    //spec body
+  });
+});
+
+
+// describe('MyCtrl2', function(){
+//   var myCtrl2;
+
+
+//   beforeEach(function(){
+//     myCtrl2 = new MyCtrl2();
+//   });
+
+
+//   it('should ....', function() {
+//     //spec body
+//   });
+// });
