2f2e49b07e80f86d8238143c33596b0379bc8ed8
John Katsnelson <voltalocke@gmail.com>
2013-03-20 19:57:29 -0700

    views being rendered non-static

--- coffee/server.coffee
+++ coffee/server.coffee
@@ -5,18 +5,53 @@ path = require 'path'
 everyauth = require 'everyauth'
 db = require './db.js'
 
+# App config
+
 app = express()
+
+app.configure () ->
+  app.pwd = path.dirname(module.uri)
+  app.set 'port', process.env.PORT || 3000
+  app.set 'views', path.join(app.pwd,'/client/app/views')
+  app.engine('.html', require('ejs').renderFile)
+  app.set('view engine', 'html');
+  app.set 'view options', 
+    layout: false
+  app.use express.compress()
+  app.use express.favicon()
+  app.use express.bodyParser()
+  app.use express.cookieParser('secretz')
+  app.use express.session()
+  app.use everyauth.middleware(app)
+  app.use express.methodOverride()
+  app.use app.router
+  app.use express.static(path.join(app.pwd, '/client/app/')) # this might be the wrong dir
+  app.use express.logger()
+
+app.get '/', (req, res) -> 
+  res.render 'layout'
+app.get '/index.html', (req, res) ->
+  res.render 'index'
+
+app.listen 3000
+
+console.log 'Go to http://localhost:3000'
+
+module.exports = app
+
+
+# RESTful end points
 
 angularBridge = new (require 'angular-bridge') app,
   urlPrefix: '/api/'
 
 angularBridge.addResource 'leagues', db.League
 
-app.configure () ->
-	app.set 'port', process.env.PORT || 3000
-	app.use app.router
+# User Auth
 
-app.use "/", express.static __dirname + '/../client/app/'
-
-http.createServer(app).listen app.get('port'), () ->
-	console.log "Express server listening on port #{app.get('port')}"
+everyauth.github
+  .appId('2bf1c804756e95d43bec')
+  .appSecret('16516757e1d87c3f13802448685375ee04674105')
+  .findOrCreateUser((session, accessToken, accessTokenExtra, githubUserMetadata) ->
+    console.log githubUserMetadata
+  ).redirectPath "/"
--- js/server.js
+++ js/server.js
@@ -15,21 +15,49 @@
 
   app = express();
 
+  app.configure(function() {
+    app.pwd = path.dirname(module.uri);
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(app.pwd, '/client/app/views'));
+    app.engine('.html', require('ejs').renderFile);
+    app.set('view engine', 'html');
+    app.set('view options', {
+      layout: false
+    });
+    app.use(express.compress());
+    app.use(express.favicon());
+    app.use(express.bodyParser());
+    app.use(express.cookieParser('secretz'));
+    app.use(express.session());
+    app.use(everyauth.middleware(app));
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express["static"](path.join(app.pwd, '/client/app/')));
+    return app.use(express.logger());
+  });
+
+  app.get('/', function(req, res) {
+    return res.render('layout');
+  });
+
+  app.get('/index.html', function(req, res) {
+    return res.render('index');
+  });
+
+  app.listen(3000);
+
+  console.log('Go to http://localhost:3000');
+
+  module.exports = app;
+
   angularBridge = new (require('angular-bridge'))(app, {
     urlPrefix: '/api/'
   });
 
   angularBridge.addResource('leagues', db.League);
 
-  app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    return app.use(app.router);
-  });
-
-  app.use("/", express["static"](__dirname + '/../client/app/'));
-
-  http.createServer(app).listen(app.get('port'), function() {
-    return console.log("Express server listening on port " + (app.get('port')));
-  });
+  everyauth.github.appId('2bf1c804756e95d43bec').appSecret('16516757e1d87c3f13802448685375ee04674105').findOrCreateUser(function(session, accessToken, accessTokenExtra, githubUserMetadata) {
+    return console.log(githubUserMetadata);
+  }).redirectPath("/");
 
 }).call(this);
