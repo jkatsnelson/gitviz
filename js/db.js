e030388355265f29d9de2aae4cc3c620c3234c9c
xiaoma <xiaoma@xiaomas-MacBook-Air-2.local>
2013-03-22 11:50:44 -0700

    adds example db schemas

--- js/db.js
+++ js/db.js
@@ -1,19 +1,58 @@
 (function() {
-  var LeagueSchema, ObjectId, Schema, db, mongoose;
+  var League, ObjectId, Schema, db, leagueSchema, mongoose, playerSchema, teamSchema;
 
   mongoose = require('mongoose');
 
-  db = mongoose.connect('mongodb://localhost/fantasygithub');
+  mongoose.connect('mongodb://localhost/fantasygithub');
+
+  db = mongoose.connection;
+
+  db.on('error', console.error.bind(console, 'connection error:'));
 
   Schema = mongoose.Schema;
 
   ObjectId = Schema.ObjectId;
 
-  LeagueSchema = new Schema({
+  leagueSchema = new Schema({
     name: String,
     teams: Array
   });
 
-  exports.League = mongoose.model('leagues', LeagueSchema);
+  teamSchema = new Schema({
+    name: {
+      type: String,
+      index: {
+        unique: true
+      }
+    },
+    players: Array
+  });
+
+  playerSchema = new Schema({
+    name: String
+  });
+
+  League = mongoose.model('League', leagueSchema);
+
+  db.once('open', function() {
+    var ncl;
+
+    ncl = new League({
+      name: 'National Codeslingers League'
+    });
+    console.log(ncl.name);
+    return ncl.save(function(err) {
+      if (err) {
+        throw err;
+      }
+      console.log("saved!");
+      return League.find({}, function(err, leagues) {
+        console.log(leagues);
+        return db.close();
+      });
+    });
+  });
+
+  exports.db = db;
 
 }).call(this);
