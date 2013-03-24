e7b260373578ad196e44a19c1a03d5c98da1d57f
John Katsnelson <voltalocke@gmail.com>
2013-03-23 22:10:09 -0700

    nearly working linked list traversal of github commits

--- github/worker.js
+++ github/worker.js
@@ -1,25 +1,55 @@
 (function() {
-  var GitHubApi, Repo, db, github;
+  var Repo, auth, author, db, getLinkedList, pageNum, repoName, repoURL, request;
 
-  GitHubApi = require('github');
+  request = require('request');
 
   db = require('../js/db.js');
 
   Repo = db.Repo;
 
-  github = new GitHubApi({
-    version: '3.0.0',
-    timeout: 5000
-  });
+  repoName = 'deface-meteor';
 
-  github.repos.getCommits({
-    user: 'gavinmcdermott',
-    repo: 'rooms'
-  }, function(err, res) {
-    if (err) {
-      throw err;
-    }
-    return console.log(res);
-  });
+  author = 'jkatsnelson';
+
+  auth = '?client_id=2bf1c804756e95d43bec&client_secret=16516757e1d87c3f13802448685375ee04674105';
+
+  repoURL = 'https://api.github.com/repos/' + author + '/' + repoName + '/commits';
+
+  pageNum = 0;
+
+  getLinkedList = function(url) {
+    var repo, res;
+
+    res = null;
+    pageNum++;
+    repo = new Repo({
+      repo: repoName,
+      author: author,
+      page: pageNum
+    });
+    return request.get(url, function(err, res, body) {
+      if (err) {
+        throw err;
+      }
+      repo.commits = body;
+      return repo.save(function(err, repo) {
+        var nextLink;
+
+        if (err) {
+          throw err;
+        }
+        if (res.headers.link) {
+          console.log(pageNum);
+          nextLink = res.headers.link.split("<")[1].split(">")[0];
+          console.log(nextLink);
+          return getLinkedList(nextLink);
+        } else {
+          throw "Done saving, last doc pageNum is " + pageNum;
+        }
+      });
+    });
+  };
+
+  getLinkedList(repoURL + auth);
 
 }).call(this);
