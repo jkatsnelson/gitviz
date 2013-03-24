c287c95a14a7c7c55d6368e2bdcfc9b8f9729f4e
John Katsnelson <voltalocke@gmail.com>
2013-03-24 13:01:53 -0700

    refactored worker

--- github/worker.coffee
+++ github/worker.coffee
@@ -1,32 +1,39 @@
 request = require 'request'
 db = require '../js/db.js'
-Repo = db.Repo
 
-repoName = 'coffee-script'
-author = 'jashkenas'
+Commit = db.Commit
+
+repoName = 'deface-meteor'
+author = 'jkatsnelson'
 auth = '?client_id=2bf1c804756e95d43bec&client_secret=16516757e1d87c3f13802448685375ee04674105'
-repoURL = 'https://api.github.com/repos/'+author+'/'+repoName+'/commits'
+repoURL = 'https://api.github.com/repos/'+author+'/'+repoName+'/commitList'
 
-pageNum = 0
-
-getLinkedList = (url) ->
-  pageNum++
-  repo = new Repo
-    repo: repoName
-    author: author
-    page: pageNum
+getCommits = (url) ->
   request.get url, (err, res, body) ->
-    throw err if err
-    repo.commits = body
-    repo.save (err, repo) ->
-      throw err if err     
-      if res.headers.link
-        if res.headers.link.split(";").length > 2
-          console.log pageNum
-          nextLink = res.headers.link.split("<")[1].split(">")[0]
-          getLinkedList(nextLink)
-        else
-          throw "List ended. Done saving. Last doc pageNum is " + pageNum
-      else
-        throw "Done saving, last doc pageNum is " + pageNum
-getLinkedList(repoURL+auth)
+    throw err if err  
+    commitList = JSON.parse body
+    console.log res
+    unless res.headers.link
+      console.log "Commit list is only one page long."
+      return saveToDB commitList
+    if res.headers.link.split(";").length > 2
+      # move to next page of the commit list
+      saveToDB commitList, res.headers.link.split("<")[1].split(">")[0]
+    else
+      saveToDB commitList
+      console.log "List ended."
+saveToDB = (commitList, nextPage) ->
+  if commitList.length
+    commitObject = commitList.pop()
+    commit = new Commit
+      repo: author + '/' + repoName
+      contributor: commitObject.author
+      message: commitObject.commit.message
+      date: commitObject.author.date
+    commit.save (err) ->
+      throw err if err
+      saveToDB commitList
+  else
+    if nextPage then getCommits nextPage else console.log 'done saving.'
+
+getCommits(repoURL+auth)
--- github/worker.js
+++ github/worker.js
@@ -1,57 +1,68 @@
 (function() {
-  var Repo, auth, author, db, getLinkedList, pageNum, repoName, repoURL, request;
+  var Commit, auth, author, db, getCommits, repoName, repoURL, request, saveToDB;
 
   request = require('request');
 
   db = require('../js/db.js');
 
-  Repo = db.Repo;
+  Commit = db.Commit;
 
-  repoName = 'coffee-script';
+  repoName = 'deface-meteor';
 
-  author = 'jashkenas';
+  author = 'jkatsnelson';
 
   auth = '?client_id=2bf1c804756e95d43bec&client_secret=16516757e1d87c3f13802448685375ee04674105';
 
-  repoURL = 'https://api.github.com/repos/' + author + '/' + repoName + '/commits';
+  repoURL = 'https://api.github.com/repos/' + author + '/' + repoName + '/commitList';
 
-  pageNum = 0;
-
-  getLinkedList = function(url) {
-    var repo;
-
-    pageNum++;
-    repo = new Repo({
-      repo: repoName,
-      author: author,
-      page: pageNum
-    });
+  getCommits = function(url) {
     return request.get(url, function(err, res, body) {
+      var commitList;
+
       if (err) {
         throw err;
       }
-      repo.commits = body;
-      return repo.save(function(err, repo) {
-        var nextLink;
-
-        if (err) {
-          throw err;
-        }
-        if (res.headers.link) {
-          if (res.headers.link.split(";").length > 2) {
-            console.log(pageNum);
-            nextLink = res.headers.link.split("<")[1].split(">")[0];
-            return getLinkedList(nextLink);
-          } else {
-            throw "List ended. Done saving. Last doc pageNum is " + pageNum;
-          }
-        } else {
-          throw "Done saving, last doc pageNum is " + pageNum;
-        }
-      });
+      commitList = JSON.parse(body);
+      console.log(res);
+      if (!res.headers.link) {
+        console.log("Commit list is only one page long.");
+        return saveToDB(commitList);
+      }
+      if (res.headers.link.split(";").length > 2) {
+        return saveToDB(commitList, res.headers.link.split("<")[1].split(">")[0]);
+      } else {
+        saveToDB(commitList);
+        return console.log("List ended.");
+      }
     });
   };
 
-  getLinkedList(repoURL + auth);
+  saveToDB = function(commitList, nextPage) {
+    var commit, commitObject;
+
+    if (commitList.length) {
+      commitObject = commitList.pop();
+      commit = new Commit({
+        repo: author + '/' + repoName,
+        contributor: commitObject.author,
+        message: commitObject.commit.message,
+        date: commitObject.author.date
+      });
+      return commit.save(function(err) {
+        if (err) {
+          throw err;
+        }
+        return saveToDB(commitList);
+      });
+    } else {
+      if (nextPage) {
+        return getCommits(nextPage);
+      } else {
+        return console.log('done saving.');
+      }
+    }
+  };
+
+  getCommits(repoURL + auth);
 
 }).call(this);
