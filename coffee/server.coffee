c215c4e2117e37c70ba243975e868eae1e2c8e41
John Katsnelson <voltalocke@gmail.com>
2013-03-25 18:57:04 -0700

    removed scripts folder and other cruft

--- coffee/server.coffee
+++ coffee/server.coffee
@@ -2,41 +2,23 @@ express = require 'express'
 routes = require 'routes'
 http = require 'http'
 path = require 'path'
-everyauth = require 'everyauth'
-db = require './db.js'
 
 # App config
 
 app = express()
 
 app.configure () ->
-  app.pwd = path.dirname(module.uri)
+  app.pwd = path.dirname module.uri
   app.set 'port', process.env.PORT || 3000
-  app.set 'views', path.join(app.pwd,'/client/app/views')
-  app.engine('.html', require('ejs').renderFile)
-  app.set('view engine', 'html');
-  app.set 'view options', 
-    layout: false
   app.use express.compress()
   app.use express.favicon()
   app.use express.bodyParser()
-  app.use express.cookieParser('secretz')
+  app.use express.cookieParser 'secretz'
   app.use express.session()
-  app.use everyauth.middleware(app)
   app.use express.methodOverride()
   app.use app.router
-  app.use express.static(path.join(app.pwd, '/client/app/')) # this might be the wrong dir
+  app.use express.static path.join app.pwd, '/app/'
   app.use express.logger()
-
-app.get '/', (req, res) -> 
-  res.render 'layout'
-app.get '/index.html', (req, res) ->
-  res.render 'index'
-app.get '/api/leagues/:id', (req, res) ->
-  id = req.params.id
-  res.json
-    user: id
-    data: "What!"
 
 app.listen 3000
 
--- js/server.js
+++ js/server.js
@@ -1,5 +1,5 @@
 (function() {
-  var app, db, everyauth, express, http, path, routes;
+  var app, express, http, path, routes;
 
   express = require('express');
 
@@ -9,49 +9,20 @@
 
   path = require('path');
 
-  everyauth = require('everyauth');
-
-  db = require('./db.js');
-
   app = express();
 
   app.configure(function() {
     app.pwd = path.dirname(module.uri);
     app.set('port', process.env.PORT || 3000);
-    app.set('views', path.join(app.pwd, '/client/app/views'));
-    app.engine('.html', require('ejs').renderFile);
-    app.set('view engine', 'html');
-    app.set('view options', {
-      layout: false
-    });
     app.use(express.compress());
     app.use(express.favicon());
     app.use(express.bodyParser());
     app.use(express.cookieParser('secretz'));
     app.use(express.session());
-    app.use(everyauth.middleware(app));
     app.use(express.methodOverride());
     app.use(app.router);
-    app.use(express["static"](path.join(app.pwd, '/client/app/')));
+    app.use(express["static"](path.join(app.pwd, '/app/')));
     return app.use(express.logger());
-  });
-
-  app.get('/', function(req, res) {
-    return res.render('layout');
-  });
-
-  app.get('/index.html', function(req, res) {
-    return res.render('index');
-  });
-
-  app.get('/api/leagues/:id', function(req, res) {
-    var id;
-
-    id = req.params.id;
-    return res.json({
-      user: id,
-      data: "What!"
-    });
   });
 
   app.listen(3000);
