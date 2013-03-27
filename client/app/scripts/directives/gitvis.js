a6bdc08a53f39cf4eb6d716a396de70f3a1c5bb8
logicmason <wei.xiaoma@gmail.com>
2013-03-27 16:56:51 -0700

    changes visualization

--- client/app/scripts/directives/gitvis.js
+++ client/app/scripts/directives/gitvis.js
@@ -3,25 +3,24 @@
 angular.module('githubleagueClientApp')
   .directive('gitvis', function () {
     return {
-      template: '<div id="mapContainer"></div>',
+      template: "<div id='mapContainer'></div>",
       restrict: 'E',
       val: '=',
       link: function postLink(scope, element, attrs) {
         // element.text('this is the gitvis directive');
- function draw(ht) {
+        function draw(ht) {
           $("#mapContainer").html("<svg id='map' xmlns='http://www.w3.org/2000/svg' width='100%' height='" + ht + "'></svg>");
           var map = d3.select("#map");
           var width = $("#map").parent().width();
           var height = ht;
 
-          // I discovered that the unscaled equirectangular map is 640x360. Thus, we
-          // should scale our map accordingly. Depending on the width ratio of the new
+          // The unscaled equirectangular map is 640x360. Depending on the width ratio of the new
           // container, the scale will be this ratio * 100. You could also use the height
           // instead. The aspect ratio of an equirectangular map is 2:1, so that's why
           // our height is half of our width.
 
-          var projector = d3.geo.equirectangular().scale((width/600)*600).translate([width/2, height/2]);
-          var path = d3.geo.path().projection(projector);
+          var projection = d3.geo.equirectangular().scale((width/600)*600).translate([width/2, height/2]);
+          var path = d3.geo.path().projection(projection);
           d3.json('../geodata/world-map.json', function(collection) {
             map.selectAll('path').data(collection.features).enter()
               .append('path')
@@ -33,38 +32,35 @@ angular.module('githubleagueClientApp')
             var dataset = locations;
             for (var i = 0; i < dataset.length; i++) {
               if ((!dataset[i].lat) || (!dataset[i].lon)) {
+                console.log('removing', dataset[i]);
                 dataset.splice(i,1);
               }
             }
 
-            var update = function(dataset) {
-              var circle = map.selectAll('circle')
-               .data(dataset);
-
-              circle.enter()
-                .append('circle')
-                .transition()
-                .duration(1000)
-                .delay(function(d,i) { return i*500; } )
-                .attr('r', function(d) {
-                 return 3;
-                })
-                .remove();
-
-              circle.attr('cx', function(d) {
-                 return projector([d.lon, d.lat])[0]; //projection(d.lon, d.lat)[0];
+            map.selectAll('circle')
+               .data(dataset)
+               .enter()
+               .append('circle')
+               .transition()
+               .attr('cx', width/2)
+               .duration(0)
+               .transition()
+               .duration(function(d,i) { return i * 300;})
+               .delay(50)
+               .attr('cx', function(d) {
+                 // if (isNaN(mercatorize(d.lon, d.lat)[0])) console.log ("Bad: ",d);
+                 return projection([d.lon, d.lat])[0]; //projection(d.lon, d.lat)[0];
                })
                .attr('cy', function(d) {
-                 return projector([d.lon, d.lat])[1]; //projection(d.lon, d.lat)[1];
+                 return projection([d.lon, d.lat])[1]; //projection(d.lon, d.lat)[1];
+               })
+               .attr('r', function(d) {
+                 return 3;
                })
                .attr('city', function(d) {
                   return d.city;
                });
 
-              circle.exit().remove();
-            }
-
-            update(dataset);
           });
         }
         draw($("#mapContainer").width()/2);
