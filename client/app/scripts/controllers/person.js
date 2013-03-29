9f92722c7cb627491b0724ae302e5ba949135bde
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-29 16:38:45 -0700

    wip

--- client/app/scripts/controllers/person.js
+++ client/app/scripts/controllers/person.js
@@ -2,19 +2,19 @@
 
 angular.module('githubleagueClientApp')
   .controller('PersonCtrl', function ($scope, $routeParams, $http) {
-    $scope.repoFound = false;
+    // $scope.repoFound = false;
 
-    $scope.searchForRepo = function(gitUser, gitRepo) {
+    // $scope.searchForRepo = function(gitUser, gitRepo) {
 
-      $scope.repoFound = false;
-      $scope.findingRepo = true;
-      $http({method: 'GET', url: '/query/' + gitUser + '/repo/' + gitRepo }).
-        success(function(data, status, headers, config) {
-          $scope.findingRepo = false;
-          $scope.repoFound = true;
-        }).
-        error(function(data, status, headers, config) {
-          console.log(status);
-        });
-    };
+    //   $scope.repoFound = false;
+    //   $scope.findingRepo = true;
+    //   $http({method: 'GET', url: '/query/' + gitUser + '/repo/' + gitRepo }).
+    //     success(function(data, status, headers, config) {
+    //       $scope.findingRepo = false;
+    //       $scope.repoFound = true;
+    //     }).
+    //     error(function(data, status, headers, config) {
+    //       console.log(status);
+    //     });
+    // };
   });
