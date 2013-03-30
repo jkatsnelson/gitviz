de077fa697cbabe3c9b661e0a010d146ac6d5814
gavinmcdermott <gavinmcdermott@gmail.com>
2013-03-30 12:06:36 -0700

    wip

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -2,12 +2,15 @@
 
 angular.module('githubleagueClientApp')
   .controller('MainCtrl', function ($scope, $routeParams, $http) {
+    $scope.leadIn = true;
     $scope.repoFound = false;
 
     $scope.searchForRepo = function(gitUser, gitRepo) {
-
+      $scope.leadIn = false
       $scope.repoFound = false;
       $scope.findingRepo = true;
+      $scope.user = gitUser;
+      $scope.repo = gitRepo;
       $http({method: 'GET', url: '/query/' + gitUser + '/repo/' + gitRepo }).
         success(function(data, status, headers, config) {
           $scope.findingRepo = false;
